eef8ca1dca75a8b88bbdd1a373fe076ef4f45e59
Zark_D <vargascardona12@gmail.com>
2023-09-12 10:46:22 -0500

    Codigo añadido

---
+++ src/com/controladores/ControladorCategorias.java
@@ -0,0 +1,122 @@
+package com.controladores;
+
+import com.excepciones.CamposVaciosException;
+import com.excepciones.DoubleInvalidoException;
+import com.excepciones.ElementoNoSeleccionadoException;
+import com.utils.ConexionUtils;
+import com.utils.Utils;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+public class ControladorCategorias {
+
+	public void insertarTabla(String nombre, String precio, String distribuidor, String categoria) {
+		if (Utils.estaVacio(nombre)
+				|| Utils.estaVacio(precio)
+				|| Utils.estaVacio(distribuidor)
+				|| Utils.estaVacio(categoria)) {
+			throw new CamposVaciosException();
+		}
+
+		if (!Utils.esDouble(precio)) {
+			throw new DoubleInvalidoException();
+		}
+
+		try {
+			PreparedStatement ps = ConexionUtils.realizarConexion().prepareStatement("INSERT INTO productos (sku, nombre, precio, distribuidor, categoria) VALUES (?, ?, ?, ? ,?)");
+			ps.setString(1, Utils.generarSku(nombre));
+			ps.setString(2, nombre);
+			ps.setString(3, precio);
+			ps.setString(4, distribuidor);
+			ps.setString(5, categoria);
+
+			ps.execute();
+
+		} catch (SQLException ex) {
+			System.err.print(ex);
+		}
+	}
+
+	public ResultSet listarTabla() {
+		try {
+			PreparedStatement ps = ConexionUtils.realizarConexion().prepareStatement("SELECT * FROM productos");
+
+			return ps.executeQuery();
+		} catch (SQLException ex) {
+			System.err.print(ex);
+		}
+		return null;
+	}
+
+	public ResultSet buscarCoincidencias(String where) {
+		try {
+			PreparedStatement ps = ConexionUtils.realizarConexion().prepareStatement("SELECT * FROM productos WHERE sku LIKE CONCAT('%',?,'%')");
+			ps.setString(1, where);
+
+			return ps.executeQuery();
+		} catch (SQLException ex) {
+			System.err.print(ex);
+		}
+		return null;
+	}
+
+	public ResultSet consultarSku(String sku) {
+		try {
+			PreparedStatement ps = ConexionUtils.realizarConexion().prepareStatement("SELECT * FROM productos WHERE sku = ?");
+			ps.setString(1, sku);
+
+			return ps.executeQuery();
+		} catch (SQLException ex) {
+			System.err.print(ex);
+		}
+		return null;
+	}
+
+	public void actualizarTabla(String sku, String nombre, String precio, String distribuidor, String categoria) {
+		if (Utils.estaVacio(sku)) {
+			throw new ElementoNoSeleccionadoException();
+		}
+
+		if (Utils.estaVacio(nombre)
+				|| Utils.estaVacio(precio)
+				|| Utils.estaVacio(distribuidor)
+				|| Utils.estaVacio(categoria)) {
+			throw new CamposVaciosException();
+		}
+
+		if (!Utils.esDouble(precio)) {
+			throw new DoubleInvalidoException();
+		}
+
+		try {
+			PreparedStatement ps = ConexionUtils.realizarConexion().prepareStatement("UPDATE productos SET nombre = ?, precio = ?, distribuidor = ?, categoria = ? WHERE sku = ?");
+			ps.setString(1, nombre);
+			ps.setString(2, precio);
+			ps.setString(3, distribuidor);
+			ps.setString(4, categoria);
+			ps.setString(5, sku);
+
+			ps.execute();
+
+		} catch (SQLException ex) {
+			System.err.print(ex);
+		}
+	}
+
+	public void eliminarTabla(String sku) {
+		if (Utils.estaVacio(sku)) {
+			throw new ElementoNoSeleccionadoException();
+		}
+
+		try {
+			PreparedStatement ps = ConexionUtils.realizarConexion().prepareStatement("DELETE FROM productos WHERE sku = ?");
+			ps.setString(1, sku);
+
+			ps.execute();
+
+		} catch (SQLException ex) {
+			System.err.print(ex);
+		}
+	}
+}
---
+++ src/com/vistas/VistaRegistrarCategorias.java
@@ -0,0 +1,90 @@
+package com.vistas;
+
+import com.controladores.ControladorGeneral;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class VistaRegistrarCategorias extends javax.swing.JFrame {
+
+	ControladorGeneral controlador;
+	VistaPrincipal vistaPrincipal;
+
+	public VistaRegistrarCategorias(VistaPrincipal vistaPrincipal) {
+		controlador = new ControladorGeneral();
+		this.vistaPrincipal = vistaPrincipal;
+
+		this.setLocationRelativeTo(null);
+		setTitle("Registrar Producto");
+
+		initComponents();
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cerrar unicamente la ventana actual
+	}
+
+	@SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        bg = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        txtNombre = new javax.swing.JTextField();
+        btnRegistrar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        bg.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Registrar Categoria");
+        bg.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, 360, 40));
+
+        jLabel4.setText("Nombre:");
+        bg.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 60, -1, -1));
+        bg.add(txtNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 60, 240, -1));
+
+        btnRegistrar.setText("Registrar");
+        btnRegistrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegistrarActionPerformed(evt);
+            }
+        });
+        bg.add(btnRegistrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 100, 320, 30));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bg, javax.swing.GroupLayout.PREFERRED_SIZE, 364, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(bg, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnRegistrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarActionPerformed
+		try {
+			controlador.insertarTabla(txtNombre.getText());
+
+			this.dispose();
+			vistaPrincipal.actualizarTabla("");
+			JOptionPane.showMessageDialog(null, "Producto registrado");
+
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(null, ex.getMessage());
+		}
+    }//GEN-LAST:event_btnRegistrarActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel bg;
+    private javax.swing.JButton btnRegistrar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JTextField txtNombre;
+    // End of variables declaration//GEN-END:variables
+}
